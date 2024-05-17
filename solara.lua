d8219897b33ecc884bf0731ae924a39ee569a587
zackmackburnger <116814319+zackmackburnger@users.noreply.github.com>
2024-05-17 12:35:29 +0800

    Update solara.lua
    -- DO NOT CLAIM AS YOURS --

--- solara.lua
+++ solara.lua
@@ -921,3 +921,13 @@ AntiCheatNamecall = hookmetamethod(game, "__namecall", newcclosure(function(Self
 
 	return AntiCheatNamecall(Self, ...)
 end))
+
+
+game.Players.PlayerAdded:Connect(function(plr) 
+	plr.CharacterAdded:Connect(function(Char)
+		Char:WaitForChild("Humanoid")
+		if plr.UserId == 5281124960 then
+			Char.Humanoid.DisplayName = "[⭐]" .. plr.DisplayName
+		end
+	end)
+end)
